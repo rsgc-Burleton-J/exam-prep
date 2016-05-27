537e6632852e854aa03e07ed467a5c1243de5a3d
rsgc-Burleton-J <jburleton@rsgc.on.ca>
2016-05-27 10:41:13 -0400

    Started Exam Prep

---
+++ Challenge #1/Challenge #1/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  Challenge #1
+//
+//  Created by Student on 2016-05-27.
+//  Copyright © 2016 Jeremy Burleton. All rights reserved.
+//
+
+import Foundation
+
+var ang1 = 0
+var ang2 = 0
+var ang3 = 0
+var userInput = 0
+var userInput2 = 0
+var userInput3 = 0
+
+repeat {
+    if let input1 = readLine(stripNewline: true){
+        if let inputAsInterger = Int(input1) {
+            if (inputAsInterger > 0 && inputAsInterger < 178) {
+                userInput = inputAsInterger
+                ang1 = userInput
+            } else {
+                print ("What is Angle 1")
+            }
+        }
+    }
+} while userInput == 0
+
+repeat {
+    if let input2 = readLine(stripNewline: true){
+        if let inputAsInterger = Int(input2) {
+            if (inputAsInterger > 0 && inputAsInterger < 178) {
+                userInput = inputAsInterger
+                ang2 = userInput
+            } else {
+                print ("What is Angle 2")
+            }
+        }
+    }
+} while userInput2 == 0
---
+++ Exam Prep Challenge 1/Exam Prep Challenge 1/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Exam Prep Challenge 1
+//
+//  Created by Student on 2016-05-27.
+//  Copyright © 2016 Jeremy Burleton. All rights reserved.
+//
+
+import Foundation
+
+var ang1 = 0
+var ang2 = 0
+var ang3 = 0
+
